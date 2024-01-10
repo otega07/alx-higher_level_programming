15ad6b2135dab20dada4f85eac5ac048b959a232
otega07 <okiriotega@gmail.com>
2024-01-10 16:20:57 +0100

    script dynamic task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv) - 1
     if x < 1:
-        print("{} arguments. ".format(x))
+        print("{} arguments.".format(x))
     elif x == 1:
         print("{} argument:".format(x))
     else:
