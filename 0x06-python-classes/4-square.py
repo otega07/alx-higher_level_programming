4b6f770c88568ac244e7c1acd6084fe60f791983
otega07 <okiriotega@gmail.com>
2024-01-23 23:26:09 +0100

    task 4 update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.size = size 
+        self.__size = size 
 
     @property
     def size(self):
