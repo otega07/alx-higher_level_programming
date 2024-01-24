fc81ffcd4434d7814b86e2797641efa2bba27e8b
otega07 <okiriotega@gmail.com>
2024-01-24 09:46:06 +0100

    task 4 update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class square."""
 
 
@@ -26,7 +25,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
