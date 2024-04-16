9cda7552ed579d8cf84990221bbfedbf8e9de57d
otega07 <okiriotega@gmail.com>
2024-04-16 09:24:11 +0100

    state orm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,22 @@
 """ Script that lists all states from the database hbtn_0e_0_usa
 """
 
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
     # Get MySQL credentials from command line arguments
     # Connect MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.connect()
+    cursor = db.cursor()
 
     #Execute the SQL query to retrieve all states sorted by id
-    c.execute("SELECT * FROM 'states' ORDER BY 'id'")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    states = cursor.fetchall()
+    for state in states:
+        if state[1][0] == "N":
+            print(state)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
