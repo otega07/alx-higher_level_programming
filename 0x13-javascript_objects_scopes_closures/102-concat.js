142615b843be31a64d1e3cf6cc975171a9624037
otega07 <okiriotega@gmail.com>
2024-04-23 19:11:08 +0100

    adv task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Concatenates files passed as arguments into output file.
+const fs = require('fs');
+const data1 = fs.readFileSync(process.argv[2], 'utf-8');
+const data2 = fs.readFileSync(process.argv[3], 'utf-8');
+fs.writeFileSync(process.argv[4], data1 + data2, 'utf-8');
